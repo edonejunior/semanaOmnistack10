c82469c5214f54b9973d40758548f37740d4f9d2
Edvaldo Domingues de Faria Junior <edvaldo.junior@santacasabp.braganca>
2020-01-16 15:05:07 -0300

    Criada a rota e estilizado nome da página

--- mobile/App.js
+++ mobile/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+//Importar as rotas
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello!!</Text>
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    fontWeight:"bold",
-    fontSize:35,
-  }
-});
