d6a97f611a5b7f119996cdaa15650be62ef91bac
Edvaldo Domingues de Faria Junior <edvaldo.junior@santacasabp.braganca>
2020-01-17 08:28:23 -0300

    status bar

--- mobile/App.js
+++ mobile/App.js
@@ -5,9 +5,10 @@ import {StatusBar} from 'react-native';
 import Routes from './src/routes'
 
 export default function App() {
+  //Nesse return estilizamos a status Bar e chamamos o arquivo routes.
   return (
     <>
-      <StatusBar  barStyle='light-content'/>
+      <StatusBar  barStyle='light-content' backgroundColor='#7D40E7'/>
       <Routes />
     </>
   );
